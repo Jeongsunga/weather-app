1dfd95173b67d5637dd260fbced2788be44a377b
Seunga Jeong <jsa3341@gmail.com>
2025-04-07 17:29:38 +0900

    use bootstrap for mobile design

--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import '../App.css';
-import { Button, ButtonGroup } from 'react-bootstrap';
+import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
 
 const WeatherButton = ({ cities, selectedCity, handleCityChange }) => {
   return (
-    <div>
-        <div className="weather-button">
-        <ButtonGroup aria-label="Basic example" className="flex-wrap justify-content-center w-100 weather-button-group">
-            <Button
-            variant={`${selectedCity === null ? "dark" : "secondary"}`}
-            onClick={() => handleCityChange("current")}>My Location</Button>
-
-            {cities.map((city) => (
+    <div className="weather-button">
+      <ButtonGroup aria-label="Basic example">
+        <Container>
+          <Row>
+            <Col lg={3} sm={12}>
               <Button
-              variant={`${selectedCity === city ? "dark" : "secondary"}`}
-              onClick={() => handleCityChange(city)}>{city}</Button>))}
-        </ButtonGroup>
-    </div>
+                variant={selectedCity === null ? "dark" : "secondary"}
+                onClick={() => handleCityChange("current")}
+              >
+                My Location
+              </Button>
+            </Col>
+            {cities.map((city) => (
+              <Col key={city} lg={3} sm={12}>
+                <Button
+                  variant={selectedCity === city ? "dark" : "secondary"}
+                  onClick={() => handleCityChange(city)}
+                >
+                  {city}
+                </Button>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </ButtonGroup>
     </div>
   )
 }
